7012e2f270c6508f9007135352b4a44d6d928274
luantafarel <luantafarel7@gmail.com>
2018-12-09 21:31:25 -0200

    Created a three way handshake between the client and server

--- transporte/server_transport.py
+++ transporte/server_transport.py
@@ -1,88 +1,64 @@
-import sys, getopt, random, select
-from socket import *
-from time import *
+# import socket programming library 
+import socket 
 
-def pksend(pkt_snd):
-    serverSocket.sendto(pkt_snd, clientAddr)
-def pkrecv():
-    return serverSocket.recvfrom(300)
+# import thread module 
+from thread import *
+import threading 
 
-serverName = ''
-serverPort = 12345
-seq = random.randint(1,1000)
-loss_cnt = 0
-prev_pkt = ''
+print_lock = threading.Lock() 
 
-serverSocket = socket(AF_INET, SOCK_DGRAM)
-serverSocket.bind(('', serverPort))
-print "UDP Server ready to receive data"
+# thread fuction 
+def threaded(c): 
+	while True: 
 
-pkt_rcv, clientAddr = pkrecv()
-print pkt_rcv
-pkt_rcv = pkt_rcv.split(';')
-if pkt_rcv[2] == "S=1":
-	ack = int(pkt_rcv[0].partition('=')[2])
-	ack = ack+1
-	respMsg = "seq="+str(seq)+";ack="+str(ack)+";SA=1;len=0"
-	prev_pkt = respMsg
-	seq = seq+1
-	pksend(respMsg)
-    
-	pkt_rcv, clientAddr = pkrecv()
-	pkt_rcv = pkt_rcv.split(';')
-	if pkt_rcv[2] == "A=1":
-		print "The connection is established!!"
-		old_seq = ack
-		ack = ack+1
-		prev_ack =  ack     
-        while 1:
-			ready = select.select([serverSocket],[],[],3)
-			if ready[0]:
-				pkt_rcv, clientAddr = pkrecv()
+		# data received from client 
+		data = c.recv(1024) 
+		if not data: 
+			print('Bye') 
+			
+			# lock released on exit 
+			print_lock.release() 
+			break
 
-				fin = pkt_rcv.split(';')
-				if fin[2]== "F=1":
-					#ack = ack+1
-					fin_ack = int(fin[0].partition('=')[2])+1
-					seq = int(fin[1].partition('=')[2])
-					pkt_snd = "seq="+str(seq)+";ack="+str(fin_ack)+";F=1;len=0"
-					pksend(pkt_snd)
-					#seq = seq+1
-					pkt_snd = "seq="+str(seq)+";ack="+str(fin_ack)+";F=1;len=0"
-					pksend(pkt_snd)
-					fin, clientAddr = pkrecv()
-					fin = fin.split(';')
-					seq = seq+1
-					if int(fin[1].partition('=')[2]) == seq: 
-						serverSocket.close()
-						exit()
+		# reverse the given string from client 
+		data = data[::-1] 
 
-				elif loss_cnt != 4:
-					print pkt_rcv
-					pkt = pkt_rcv.split(';')
-					new_seq = int(pkt[0].partition('=')[2])
-					if prev_ack<new_seq: 
-						pksend(prev_pkt)				
-						continue
-					elif prev_ack>new_seq:
-						continue
-					ack = ack + int(pkt[3].partition('=')[2])
-					respMsg = "seq="+str(seq)+";ack="+str(ack)+";A=1;len=0"
-					loss_cnt= loss_cnt+1
-					if loss_cnt == 6: #random.randint(5,10):
-						print "This packet is delayed"
-						loss_cnt = 0
-						sleep(5)
-					if old_seq<new_seq:
-						pksend(respMsg)
-					prev_pkt = respMsg
-					prev_ack = ack
-					old_seq = new_seq
-				else:
-					loss_cnt += 1
+		# send back reversed string to client 
+		c.send(data) 
+
+	# connection closed 
+	c.close() 
 
 
-	else:
-				print "Connection establishment failed!!"	
+def Main(): 
+	host = "" 
 
-serverSocket.close()
+	# reverse a port on your computer 
+	# in our case it is 12345 but it 
+	# can be anything 
+	port = 12345
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+	s.bind((host, port)) 
+	print("socket binded to post", port) 
+
+	# put the socket into listening mode 
+	s.listen(5) 
+	print("socket is listening") 
+
+	# a forever loop until client wants to exit 
+	while True: 
+
+		# establish connection with client 
+		c, addr = s.accept() 
+
+		# lock acquired by client 
+		print_lock.acquire() 
+		print('Connected to :', addr[0], ':', addr[1]) 
+
+		# Start a new thread and return its identifier 
+		start_new_thread(threaded, (c,)) 
+	s.close() 
+
+
+if __name__ == '__main__': 
+	Main() 
